b5ebb34f8c708f48c5ee2ca8787aca938b8e2154
LeandroDukievicz <ldukie@icloud.com>
2024-12-05 16:36:30 -0300

    🧹 CHORE: added assets and init structure of the project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    // Adicione os caminhos para os arquivos que contêm classes do Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
